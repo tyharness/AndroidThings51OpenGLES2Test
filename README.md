ee6c7e8014c8f404bea47dafdb577e31ad3f5459
tyharness <colemanbicycle@gmail.com>
2017-08-28 21:57:15 +0100

    inital upload

---
+++ README.md
@@ -0,0 +1,89 @@
+Example opengles2 Android app for phone and Android Things Device Developer Preview 4.1 and 5.1 on the rpi3
+--------------------------------------------------------------------------------------
+
+Please note this app comes in 3 flavours: phone, Android Things 41 and Android Things 51.
+
+demo.png
+
+At this stage the app is not running on 51.  Android 41 is not hardware accelerated and therefore unlikey to work but made
+for completeness.  The phone flavour indicates the code is valid in a general sense.
+
+Just playing about trying to learn opengles2 and Android Things on the Raspberry Pi.  Any help or input
+is much appreciated.  It's a very simple app to display Maxwell's colour triangle.
+ 
+
+Ty Harness August 2017
+
+
+
+I've got my phone plugged into the USB and the debugging enabled.
+
+To connect to Android Things:
+adb connect 192.168.0.10
+
+adb devices -l
+List of devices attached
+192.168.0.10:5555      device product:   iot_rpi3 model:iot_rpi3 device:rpi3
+cfa60340               device usb:1-1
+
+
+
+
+---------------------------------------------------------------------------------------
+Build via Android SDK the CLI:
+
+./gradlew assembleDebug
+
+----------------------------------------------------------------------------------------
+Installation and use:
+
+Use your IP address and PhoneID not mine.
+
+
+Android Phone Flavour:
+
+
+adb -s cfa60340 install -r app/build/outputs/apk/app-phone-debug.apk
+
+
+adb -s cfa60340 logcat | grep MainActivity
+
+
+-------------------------------------------------------------
+
+Android Things 51 Flavour:
+
+adb -s 192.168.0.10 install -r app/build/outputs/apk/app-things51-debug.apk
+
+adb -s 192.168.0.10 shell am start example.androidthings.myopengles2Test/.MainActivity
+
+adb -s 192.168.0.10  logcat | grep MainActivity
+
+--------------------------------------------------------------
+
+Android Things 41 Flavour:
+
+same as above
+
+adb -s 192.168.0.10 install -r app/build/outputs/apk/app-things41-debug.apk
+
+
+---------------------------------------------------------------
+Useful Commands for Android Things:
+
+See what OEM apps have been installed:
+
+adb -s 192.168.0.10 shell pm list packages -3
+
+How to unistall:
+adb -s 192.168.0.10 uninstall example.androidthings.myopengles2Test
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/example/androidthings/myopengles2Test/MainActivity.java
@@ -0,0 +1,109 @@
+package example.androidthings.myopengles2Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.os.Build;
+
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    
+    
+  
+    private GLSurfaceView mGLSurfaceView;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate");
+        
+        
+    mGLSurfaceView = new GLSurfaceView(this);
+ 
+    
+    
+    final boolean isHardwareAccelerationSupported = mGLSurfaceView.isHardwareAccelerated();
+    
+    if (isHardwareAccelerationSupported)
+    {  
+       Log.d(TAG,  "hardware Acceleration is supported" ); 
+    }else{
+       Log.d(TAG,  "hardware Acceleration is not supported " + mGLSurfaceView.toString() );
+    }
+    
+    
+    
+    
+    // Check if the system supports OpenGL ES 2.0.
+    final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    final ConfigurationInfo Info = activityManager.getDeviceConfigurationInfo();
+   
+    //Bypass but app may crash
+    //final boolean isGLES20supported = Info.reqGlEsVersion >= 0x20000  ||  Build.MODEL.contains("iot_rpi3");
+    
+    final boolean isGLES20supported = Info.reqGlEsVersion >= 0x20000 ;
+    
+  
+    
+    
+    if (isGLES20supported)
+    {       
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setRenderer(new MyGLrenderer());
+    }
+    else
+    {           
+        Log.d(TAG, "OpenGLES 2 is not supported");       
+    }
+
+        Log.d(TAG,  Info.toString() );
+        
+        Log.d(TAG,  Integer.toBinaryString(Info.reqGlEsVersion) );
+        Log.d(TAG,  Integer.toHexString(Info.reqGlEsVersion) );
+        Log.d(TAG,  Info.getGlEsVersion ());
+         
+        Log.d(TAG,  Build.FINGERPRINT );       
+        Log.d(TAG,  Build.BOARD );
+        Log.d(TAG,  Build.BOOTLOADER );
+        Log.d(TAG,  Build.MODEL );
+    
+       
+    
+    
+       setContentView(mGLSurfaceView);
+        
+    }
+
+
+   
+    
+@Override
+protected void onResume()
+{
+    // The activity must call the GL surface view's onResume() on activity onResume().
+    super.onResume();
+    //mGLSurfaceView.onResume();
+}
+ 
+@Override
+protected void onPause()
+{
+    // The activity must call the GL surface view's onPause() on activity onPause().
+    super.onPause();
+   // mGLSurfaceView.onPause();
+}
+   
+    
+ @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+}
